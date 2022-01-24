64ff711844e02e70d0fc61854068191ece4ae3d1
caglakahriman <nc.kahriman7@gmail.com>
2022-01-24 15:30:22 +0300

    get-next-line

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nkahrima <nkahrima@student.42istanbul.com  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/19 09:47:31 by nkahrima          #+#    #+#             */
+/*   Updated: 2022/01/19 16:34:05 by nkahrima         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*first_line(int fd, char *buffer)
+{
+	char	*buff;
+	int		rd_byte;
+
+	rd_byte = 1;
+	buff = malloc (sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buff)
+		return (NULL);
+	while (!find_nl_char(buffer) && rd_byte != 0)
+	{
+		rd_byte = read (fd, buff, BUFFER_SIZE);
+		if (rd_byte == -1)
+		{
+			free (buffer);
+			free (buff);
+			return (NULL);
+		}
+		buff[rd_byte] = '\0';
+		buffer = strjoin(buffer, buff);
+	}
+	free (buff);
+	return (buffer);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	static char	*buffer;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	buffer = first_line(fd, buffer);
+	if (buffer == NULL)
+		return (NULL);
+	line = get_new_line(buffer);
+	buffer = get_new_buffer(buffer);
+	return (line);
+}
---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nkahrima <nkahrima@student.42istanbul.com  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/19 09:47:52 by nkahrima          #+#    #+#             */
+/*   Updated: 2022/01/19 16:34:22 by nkahrima         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+
+size_t	ft_strlen(char *buf);
+char	*get_next_line(int fd);
+char	*get_new_line(char *buffer);
+char	*first_line(int fd, char *buffer);
+char	*strjoin(char *buffer, char *new_buffer);
+char	*get_new_buffer(char *buffer);
+int		find_nl_char(char *buffer);
+
+#endif
